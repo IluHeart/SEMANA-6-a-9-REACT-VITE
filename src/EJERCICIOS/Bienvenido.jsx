f1916b3b753006becdde3df5a66b3d4c8d49ea6a
IliesGuellaf <108081986+IluHeart@users.noreply.github.com>
2023-10-13 19:26:48 +0200

    Ejercicio 23 React Vite props

--- src/EJERCICIOS/Bienvenido.jsx
+++ src/EJERCICIOS/Bienvenido.jsx
@@ -1,13 +1,13 @@
 import Edad from "./Edad"
 
 const Bienvenido = (props) => {
-  const isAgeValid = props.age > 18 && props.age < 65;
-  console.log(isAgeValid);
+   const isAgeValid = props.age > 18 && props.age < 65;
+   console.log(isAgeValid);
 
   return (
     <div>
       <p>Bienvenido, {props.name || <strong>HipHop</strong>}</p>
-      {isAgeValid && <Edad age={props.age} />}
+       {isAgeValid && <Edad age={props.age} />}
     </div>
   );
 };
