ddc51ce70b7f740073da3d168379ee0f3d985f55
IliesGuellaf <108081986+IluHeart@users.noreply.github.com>
2023-10-31 15:17:13 +0100

    Ejercicio 51 React Vite Refs

--- src/EJERCICIOS/FirstMount.jsx
+++ src/EJERCICIOS/FirstMount.jsx
@@ -3,17 +3,17 @@ import { useEffect, useRef } from "react";
 const FirstMount = () => {
   const isMountRef = useRef(false);
 
-useEffect(() => {
-  if (!isMountRef.current) {
-    console.log("El componente se ha montado por primera vez");
-    isMountRef.current = true;
-  }
-}, []);
+  useEffect(() => {
+    if (!isMountRef.current) {
+      console.log("El componente se ha montado por primera vez");
+      isMountRef.current = true;
+    }
+  }, []);
 
-return (
-  <div>
-    <p>Ejercicio 51 ref</p>
-  </div>
-);
+  return (
+    <div>
+      <p>Ejercicio 51 ref</p>
+    </div>
+  );
 };
 export default FirstMount;
